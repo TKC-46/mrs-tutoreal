0f1b0ae41b6ef78ac68f61dd91ecd83730367bb6
小川剛史 <q2bh3jv@gmail.com>
2025-05-01 17:44:56 +0900

    エラーを解消

--- src/main/java/mrs/domain/model/ReservableRoom.java
+++ src/main/java/mrs/domain/model/ReservableRoom.java
@@ -18,4 +18,29 @@ public class ReservableRoom implements Serializable {
 	@JoinColumn(name = "room_id", insertable = false, updatable = false)
 	@MapsId("roomId")
 	private MeetingRoom meetingRoom;
+	
+	public ReservableRoom(ReservableRoomId reservableRoomId) {
+		this.reservableRoomId = reservableRoomId;
+	}
+	
+	public ReservableRoom() {
+	}
+
+	public ReservableRoomId getReservableRoomId() {
+		return reservableRoomId;
+	}
+
+	public void setReservableRoomId(ReservableRoomId reservableRoomId) {
+		this.reservableRoomId = reservableRoomId;
+	}
+
+	public MeetingRoom getMeetingRoom() {
+		return meetingRoom;
+	}
+
+	public void setMeetingRoom(MeetingRoom meetingRoom) {
+		this.meetingRoom = meetingRoom;
+	}
+	
+	
 }
